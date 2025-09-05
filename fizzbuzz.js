29098974f3576226b0f295260f6b6e48c227aebf
Luke Stuart <luke.stuart@softwire.com>
2025-09-05 09:56:47 +0100

    Part 2

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -3,11 +3,16 @@ function fizzbuzz() {
     console.log("Hello, World!");
 
     // Put your code here...
-    for (let i = 1; i <= 100; i++) {
-        let output = "";
-        if (i % 3 === 0) output += "Fizz";
-        if (i % 5 === 0) output += "Buzz";
-        console.log(output || i);
+    for (let i = 1; i <= 255; i++) {
+        let output = [];
+        let multipleOfEleven = i % 11 === 0;
+        if (i % 3 === 0 && !multipleOfEleven) output.push("Fizz");
+        if (i % 13 === 0) output.push("Fezz");
+        if (i % 5 === 0 && !multipleOfEleven) output.push("Buzz");
+        if (i % 7 === 0 && !multipleOfEleven) output.push("Bang");
+        if (multipleOfEleven) output.push("Bong");
+        if (i % 17 === 0) output.reverse();
+        console.log(output.join("") || i);
     }
 }
 
